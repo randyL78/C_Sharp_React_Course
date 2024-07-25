42711421357d42052150dc022de65077bd9135f7
Randy Layne <randydavidl78@gmail.com>
2024-07-24 22:09:05 -0400

    Add first request from client to api to fetch and display activities

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,25 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Header, List, ListItem} from "semantic-ui-react";
 
 function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => setActivities(response.data))
+  }, []);
+
   return (
-    <h1>Reactivities</h1>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+        {activities.map((activity: any) => (
+          <ListItem key={activity.id}>{activity.title}</ListItem>
+        ))}
+      </List>
+    </div>
   )
 }
 
